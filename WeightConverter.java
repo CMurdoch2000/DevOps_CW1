fd331cc0b604a5a4133655903c39606c5f547356
CMurdoch2000 <loudcallum@gmail.com>
2026-08-05 12:07:56 +0000

    Fixed missing arguments, number formats and case sensitivity issues

--- WeightConverter.java
+++ WeightConverter.java
@@ -1,22 +1,43 @@
-import java.util.*;
 import java.text.DecimalFormat;
 
 public class WeightConverter {
     public static void main(String[] args) {
-        double weight;
+        
+        // Fix for Task 5a Checking for missing arguments
+        if (args.length < 2) {
+            System.out.println("Error Please provide a weight and a unit.");
+            System.out.println("Example java WeightConverter 25 kilograms");
+            System.exit(0);
+        }
+
+        double weight = 0;
         DecimalFormat f = new DecimalFormat("##.##");
-        weight = Double.parseDouble(args[0]);
-        String unit = args[1];
+
+        // Fix for Task 5a Checking for incorrect number formats
+        try {
+            weight = Double.parseDouble(args[0]);
+        } catch (NumberFormatException e) {
+            System.out.println("Error Please enter a valid number for the weight.");
+            System.exit(0);
+        }
+
+        // Fix for Task 5b Forcing the input to lower case to handle capital letters
+        String unit = args[1].toLowerCase();
+
+        // Declaring variables here fixes SonarQube scope warnings
+        double pounds = 0;
+        double grams = 0;
+        double kilograms = 0;
 
         switch(unit) {
             case "kilograms":
-                double pounds = weight * 2.205;
-                double grams = weight * 1000;
+                pounds = weight * 2.205;
+                grams = weight * 1000;
                 System.out.println(weight + " Kilograms = " + f.format(pounds) + " Pounds");
                 System.out.println(weight + " Kilograms = " + f.format(grams) + " Grams");
                 break;
             case "pounds":
-                double kilograms = weight * 0.454;
+                kilograms = weight * 0.454;
                 grams = kilograms * 1000;
                 System.out.println(weight + " Pounds = " + f.format(kilograms) + " Kilograms");
                 System.out.println(weight + " Pounds = " + f.format(grams) + " Grams");
@@ -27,7 +48,11 @@ public class WeightConverter {
                 System.out.println(weight + " Grams = " + f.format(kilograms) + " Kilograms");
                 System.out.println(weight + " Grams = " + f.format(pounds) + " Pounds");
                 break;
+            default:
+                System.out.println("Error Unknown unit. Please use kilograms, pounds or grams.");
+                System.exit(0);
         }
+        
         System.out.println("Thank you for using the weight converter.");
     }
 }
